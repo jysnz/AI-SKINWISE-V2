047462a2e8f85afcb567fea9d9f3e8aadd802eb4
Jayson Dela Cruz <delacruzjayson020305@gmail.com>
2025-11-05 21:58:45 +0800

    Initial commit

---
+++ android/app/src/main/java/aiskinwise/com/ai_skinwise_v2/MainActivity.java
@@ -0,0 +1,6 @@
+package aiskinwise.com.ai_skinwise_v2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/java/com/example/ai_skinwise_v2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ai_skinwise_v2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
